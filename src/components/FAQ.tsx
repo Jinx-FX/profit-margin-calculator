d5d76e5668ca15ba6917e0931750d81baef52b83
Jinx-FX <2674802603@qq.com>
2023-06-06 17:01:36 +0800

    fix:FAQ

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -26,14 +26,7 @@ function FAQ() {
             and discounts). Then divide this figure by net sales, to calculate
             the gross profit margin in a percentage. <br />
             <br />
-            <a
-              href="https://www.youtube.com/@codeofrelevancy"
-              target="_blank"
-              className="text-brand">
-              Code of Relevancy
-            </a>
-            's free profit margin calculator does it for you, but you can also
-            use the following formula:
+            You can use the following formula:
             <br />
             Step 1: X (Net sales) - Y (COGS) = Z<br />
             Step 2: Z / X (Net sales) = % Gross profit margin
@@ -54,23 +47,6 @@ function FAQ() {
                 profit margin
               </li>
             </ol>
-          </div>
-        </li>
-        <li>
-          <p className="text-lg font-medium leading-6 text-gray-900 dark:text-gray-100">
-            How does the profit margin calculator work?
-          </p>
-          <div className="mt-2 text-base leading-6 text-gray-500 dark:text-gray-400">
-            <a
-              href="https://www.youtube.com/@codeofrelevancy"
-              target="_blank"
-              className="text-brand">
-              Code of Relevancy
-            </a>
-            ’s free profit margin calculator is fast and easy to use. It simply
-            takes the markup percentage you enter, adds that in decimal format
-            to the cost, and then gives you a sale price which reflects the
-            markup.
           </div>
         </li>
       </ul>
